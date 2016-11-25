4c6e830ce433f17b2433ae67caebd7cd40198db6
Meeji <sebastian.brabiner@gmail.com>
2016-11-25 18:55:31 +0000

    Moved ecs container macro back to container module

--- src/ecs/container.rs
+++ src/ecs/container.rs
@@ -1,14 +1,3 @@
-/*
-create_container(
-    with_systems {
-        has_name_key   => has_name,
-        has_health_key => has_health,
-    }
-)
-
-
-*/
-
 pub trait FromEcs<E: ContainsSystem> where Self: Sized {
     fn from_ecs(ecs: &E) -> &Self;
 }
@@ -28,46 +17,53 @@ pub trait ContainsMutSystem: ContainsSystem {
 }
 
 
-// #[macro_export]
+#[macro_export]
 macro_rules! create_container {
     (with_systems {
-        $($sys_id:ident => $sys_type:ty),+
+        $($sys_id:ident => $sys_type:ty = $cmp_type:ty),+
     }) => (
 
-    pub struct EcsContainer<F : CreatesEntities> {
+    pub struct EcsContainer<F: CreatesEntities> {
         entity_factory: F,
         $(pub $sys_id: $sys_type,)+
     }
 
-    impl<F : CreatesEntities> EcsContainer<F> {
-        pub fn new_entity(&mut self) -> Entity {
-            self.entity_factory.new_entity()
+    impl<F: CreatesEntities> EcsContainer<F> {
+        pub fn new_entity(&mut self) -> EntityConfiguration<Self> {
+            let entity = self.entity_factory.new_entity();
+            self.configure_entity(entity)
         }
+
+        pub fn configure_entity(&mut self, entity: Entity) -> EntityConfiguration<Self> {
+            EntityConfiguration::new(self, entity)
+        }
+    }
+
+    impl<F: CreatesEntities> ContainsSystem for EcsContainer<F> {
+        fn get_system<S>(&self) -> &S
+            where S: FromEcs<Self> {
+                FromEcs::from_ecs(self)
+            }
+    }
+
+    impl<F : CreatesEntities> ContainsMutSystem for EcsContainer<F> {
+        fn get_system_mut<S>(&mut self) -> &mut S
+            where S: FromEcsMut<Self> {
+                FromEcsMut::from_ecs_mut(self)
+            }
     }
 
     $(
-        impl<F : CreatesEntities> Deref for EcsContainer<F> {
-            type Target = $sys_type;
-
-            fn deref(&self) -> &$sys_type {
-                &self.$sys_id
+        impl<F: CreatesEntities> FromEcs<EcsContainer<F>> for $sys_type {
+            fn from_ecs(ecs: &EcsContainer<F>) -> &$sys_type {
+                &ecs.$sys_id
             }
         }
 
-        impl<F : CreatesEntities> DerefMut for EcsContainer<F> {
-            fn deref_mut(&mut self) -> &mut $sys_type {
-                &mut self.$sys_id
+        impl<F : CreatesEntities> FromEcsMut<EcsContainer<F>> for $sys_type {
+            fn from_ecs_mut(ecs: &mut EcsContainer<F>) -> &mut $sys_type {
+                &mut ecs.$sys_id
             }
         }
-    )+
-
-    pub struct EntityConfiguration {
-        entity: Entity,
-    }
-
-    impl EntityConfiguration {
-        pub fn new(entity: Entity) -> EntityConfiguration {
-            EntityConfiguration { entity: entity }
-        }
-    })
+    )+)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,60 +4,10 @@ extern crate something;
 use something::ecs::*;
 use something::components::*;
 
-macro_rules! create_container {
-    (with_systems {
-        $($sys_id:ident => $sys_type:ty),+
-    }) => (
-
-    pub struct EcsContainer<F: CreatesEntities> {
-        entity_factory: F,
-        $(pub $sys_id: $sys_type,)+
-    }
-
-    impl<F: CreatesEntities> EcsContainer<F> {
-        pub fn new_entity(&mut self) -> EntityConfiguration<Self> {
-            let entity = self.entity_factory.new_entity();
-            self.configure_entity(entity)
-        }
-
-        pub fn configure_entity(&mut self, entity: Entity) -> EntityConfiguration<Self> {
-            EntityConfiguration::new(self, entity)
-        }
-    }
-
-    impl<F: CreatesEntities> ContainsSystem for EcsContainer<F> {
-        fn get_system<S>(&self) -> &S
-            where S: FromEcs<Self> {
-                FromEcs::from_ecs(self)
-            }
-    }
-
-    impl<F : CreatesEntities> ContainsMutSystem for EcsContainer<F> {
-        fn get_system_mut<S>(&mut self) -> &mut S
-            where S: FromEcsMut<Self> {
-                FromEcsMut::from_ecs_mut(self)
-            }
-    }
-
-    $(
-        impl<F: CreatesEntities> FromEcs<EcsContainer<F>> for $sys_type {
-            fn from_ecs(ecs: &EcsContainer<F>) -> &$sys_type {
-                &ecs.$sys_id
-            }
-        }
-
-        impl<F : CreatesEntities> FromEcsMut<EcsContainer<F>> for $sys_type {
-            fn from_ecs_mut(ecs: &mut EcsContainer<F>) -> &mut $sys_type {
-                &mut ecs.$sys_id
-            }
-        }
-    )+)
-}
-
 create_container!(
     with_systems {
-        has_name => System<HasName>,
-        has_health => System<HasHealth>
+        has_name   => System<HasName> = HasName,
+        has_health => System<HasHealth> = HasHealth
     }
 );
 
@@ -70,8 +20,8 @@ fn main() {
 
     // Create entity, and configure components
     let geralt: Entity = container.new_entity()
-        .with_component::<HasName, System<HasName>>(HasName::new("Geralt"))
-        .with_component::<HasHealth, System<HasHealth>>(HasHealth::new(100))
+        .with_component::<_, System<_>>(HasName::new("Geralt"))
+        .with_component::<_, System<_>>(HasHealth::new(100))
         .into();
 
     // Print Geralt's ID.
